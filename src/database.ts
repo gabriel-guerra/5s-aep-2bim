6d9401df589be9853c2d60f7cfb28624417169a4
Gabriel S. Guerra <159430864+gabriel-guerra@users.noreply.github.com>
2024-06-11 10:22:31 +0000

    alterações conexão db

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config'
 
 const sequelize = new Sequelize(`postgres://${process.env.ADM_DB}:${process.env.ADM_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DATABASE_NAME}`, {dialect: 'postgres'});
 
